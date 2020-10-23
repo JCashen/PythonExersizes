087446bc8ca9ffc8557070b0ac77b1d5cb09ed98
Josh Cashen <72808022+JCashen@users.noreply.github.com>
2020-10-23 14:53:55 +0100

    Create Compare.py

---
+++ Compare.py
@@ -0,0 +1,11 @@
+ATTEMPT 1
+print('input first word')
+str1 = input()
+print('input second word')
+str2 = input()
+try:
+    str1.index(str2)
+except ValueError:
+    print('False')
+else:
+    print('True')
