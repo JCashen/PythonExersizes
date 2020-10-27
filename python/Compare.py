4bbd1a862302336ab7a10f0bd171badec043756a
Josh Cashen <72808022+JCashen@users.noreply.github.com>
2020-10-27 16:25:01 +0000

    Update Compare.py

--- python/Compare.py
+++ python/Compare.py
@@ -9,3 +9,20 @@ except ValueError:
     print('False')
 else:
     print('True')
+
+    #correct way
+    def near(string_parent, string_child):
+    list_parent = list(string_parent)
+    list_child = list(string_child)
+    for index in range(len(list_parent)):
+        item_to_remove = list_parent[index]
+        del list_parent[index]
+        if list_parent == list_child:
+            return True
+        list_parent.insert(index, item_to_remove)
+    return False
+
+word1 = input('Enter first word: ')
+word2 = input('enter second word: ')
+
+print(near(word1, word2))
